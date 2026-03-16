d6cf9276d2496c3683815cbab122b9e61131c525
ACT-STUDENT <ACT-STUDENT@LAB2-PC37>
2026-03-16 17:34:27 +0800

    Add project files.

---
+++ Proel4wProject_EasyRent/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proel4wProject_EasyRent.Models;
+
+namespace Proel4wProject_EasyRent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Proel4wProject_EasyRent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proel4wProject_EasyRent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
