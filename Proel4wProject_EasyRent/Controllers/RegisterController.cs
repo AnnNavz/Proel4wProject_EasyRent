6ef092b34c901a7aff17f7ebebe3231c5dce26c9
AnnNavz <ccs.annshirleynavasca@gmail.com>
2026-04-05 00:26:02 +0800

    Register

---
+++ Proel4wProject_EasyRent/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proel4wProject_EasyRent.Data;
+using Proel4wProject_EasyRent.Models;
+using Proel4wProject_EasyRent.Services;
+
+namespace Proel4wProject_EasyRent.Controllers
+{
+	public class RegisterController : Controller
+	{
+		private readonly Proel4wProject_EasyRentContext _context;
+
+		public RegisterController(Proel4wProject_EasyRentContext context)
+		{
+			_context = context;
+		}
+
+		// GET: Account/Register
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		// Added ConfirmPassword to the Bind attribute
+		public async Task<IActionResult> Register([Bind("UserFirstName,UserLastName,UserEmail,Password,ConfirmPassword")] Users users)
+		{
+			if (ModelState.IsValid)
+			{
+				// 1. Hash the password
+				string hash = HashingServices.HashData(users.Password);
+				users.Password = hash;
+				users.ConfirmPassword = hash; // Set both to the hashed value
+
+				// 2. Automatically assign RoleId 2 (Customer)
+				users.RoleId = 2;
+
+				_context.Add(users);
+				await _context.SaveChangesAsync();
+
+				// Redirect to login or home after successful registration
+				return RedirectToAction("Index", "Home");
+			}
+			// If we got this far, something failed; redisplay form with errors
+			return View(users);
+		}
+	}
+}
